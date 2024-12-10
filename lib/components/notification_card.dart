03ae08c59ce94964d6d97189de1268081dd0a770
Tan Nguyen <tann3946@gmail.com>
2024-12-10 21:02:18 +0700

    Update notification_card.dart

--- lib/components/notification_card.dart
+++ lib/components/notification_card.dart
@@ -24,7 +24,8 @@ class NotificationCard extends StatelessWidget {
       elevation: 2,
       color: status == 'UNREAD' ? Colors.yellow[100] : Colors.white,
       child: ListTile(
-        title: Text(type, style: const TextStyle(fontWeight: FontWeight.bold)),
+        title: Text(getTitleByType(type),
+            style: const TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -44,5 +45,19 @@ class NotificationCard extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  String getTitleByType(String type) {
+    switch (type) {
+      case 'ABSENCE':
+        return 'ĐIỂM DANH';
+      case 'ACCEPT_ABSENCE_REQUEST':
+        return 'CHẤP NHẬN ĐƠN XIN NGHỈ';
+      case 'REJECT_ABSENCE_REQUEST':
+        return 'TỪ CHỐI ĐƠN XIN NGHỈ';
+      case 'ASSIGNMENT_GRADE':
+        return 'ĐIỂM BÀI TẬP';
+    }
+    return 'THÔNG BÁO';
   }
 }
