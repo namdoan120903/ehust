ed6a51109646999b815177272e16a0b9dadb0107
Tan Nguyen <tann3946@gmail.com>
2024-12-10 21:22:50 +0700

    Update attend_record_lecturer.dart

--- lib/components/attend_record_lecturer.dart
+++ lib/components/attend_record_lecturer.dart
@@ -73,41 +73,45 @@ class AttendanceDetailPage extends StatefulWidget {
 }
 
 class _AttendanceDetailPageState extends State<AttendanceDetailPage> {
-  final ScrollController _scrollController = ScrollController();
   int _currentPage = 0;
   final int _pageSize = 18;
   bool _isLoading = false;
+  bool _hasMoreData = true; // Track if more data is available
+  late ScrollController _scrollController;
   late RollCallProvider rollCallProvider;
 
   @override
   void initState() {
     super.initState();
-
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
     WidgetsBinding.instance.addPostFrameCallback((_) {
       rollCallProvider = Provider.of<RollCallProvider>(context, listen: false);
       _fetchAttendanceData();
     });
+  }
 
-    // Scroll listener for pagination
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-              _scrollController.position.maxScrollExtent &&
-          !_isLoading) {
-        _fetchAttendanceData();
-      }
-    });
+  void _onScroll() {
+    if (_scrollController.position.pixels >=
+            _scrollController.position.maxScrollExtent - 200 &&
+        !_isLoading &&
+        _hasMoreData) {
+      _fetchAttendanceData();
+    }
   }
 
   Future<void> _fetchAttendanceData() async {
-    if (_isLoading) return;
+    if (_isLoading || !_hasMoreData) return;
 
     setState(() {
       _isLoading = true;
     });
 
     try {
+      // Save the initial length of the records
       int initialSize = rollCallProvider.recordForLecturer.length;
 
+      // Fetch new data
       await rollCallProvider.getAttendanceList(
         widget.classId,
         widget.date,
@@ -115,11 +119,13 @@ class _AttendanceDetailPageState extends State<AttendanceDetailPage> {
         _pageSize,
       );
 
+      // Check if new data was added
       int finalSize = rollCallProvider.recordForLecturer.length;
 
-      // Increment page if new data was loaded
       if (finalSize > initialSize) {
-        _currentPage++;
+        _currentPage++; // Increment the page for the next fetch
+      } else {
+        _hasMoreData = false; // No more data available
       }
     } catch (e) {
       print("Error fetching attendance data: $e");
@@ -139,145 +145,134 @@ class _AttendanceDetailPageState extends State<AttendanceDetailPage> {
 
     return Scaffold(
       appBar: MyAppBar(check: true, title: "EHUST-LECTURER"),
-      body: _isLoading && attendanceRecords.isEmpty
-          ? const Center(
-              child:
-                  CircularProgressIndicator()) // Centered loader for initial load
-          : Column(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              controller: _scrollController,
+              padding: const EdgeInsets.all(16.0),
               children: [
-                Expanded(
-                  child: ListView(
-                    controller: _scrollController,
-                    padding: const EdgeInsets.all(16.0),
-                    children: [
-                      Table(
-                        border: TableBorder.all(color: Colors.black),
-                        columnWidths: const {
-                          0: FractionColumnWidth(0.3), // ID column
-                          1: FractionColumnWidth(0.5), // Name column
-                          3: FractionColumnWidth(0.2), // Attendance column
-                        },
-                        children: [
-                          // Table headers
-                          TableRow(
-                            decoration: BoxDecoration(color: Colors.grey[300]),
-                            children: const [
-                              TableCell(
-                                verticalAlignment:
-                                    TableCellVerticalAlignment.middle,
-                                child: Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(8),
-                                    child: Text("MSSV",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                  ),
-                                ),
-                              ),
-                              TableCell(
-                                verticalAlignment:
-                                    TableCellVerticalAlignment.middle,
-                                child: Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(8),
-                                    child: Text("Họ và Tên",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                  ),
-                                ),
-                              ),
-                              TableCell(
-                                verticalAlignment:
-                                    TableCellVerticalAlignment.middle,
-                                child: Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(8),
-                                    child: Text("Trạng thái",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          // Table rows with student data
-                          for (var student in attendanceRecords)
-                            TableRow(
-                              children: [
-                                TableCell(
-                                  verticalAlignment:
-                                      TableCellVerticalAlignment.middle,
-                                  child: Center(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: Text(student.studentId,
-                                          textAlign: TextAlign.center),
-                                    ),
-                                  ),
-                                ),
-                                TableCell(
-                                  verticalAlignment:
-                                      TableCellVerticalAlignment.middle,
-                                  child: Center(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: Text(
-                                          classProvider
-                                              .findNameById(student.studentId),
-                                          textAlign: TextAlign.center),
-                                    ),
-                                  ),
-                                ),
-                                TableCell(
-                                  verticalAlignment:
-                                      TableCellVerticalAlignment.middle,
-                                  child: Center(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: GestureDetector(
-                                        onTap: () async {
-                                          String newStatus = student.status ==
-                                                  'PRESENT'
-                                              ? 'EXCUSED_ABSENCE'
-                                              : 'PRESENT'; // Toggle the status
-                                          await rollCallProvider
-                                              .setAttendanceStatus(
-                                                  student.attendanceId,
-                                                  newStatus);
-                                          setState(() {
-                                            student.status =
-                                                newStatus; // Set the new status
-                                          });
-                                        },
-                                        child: Icon(
-                                          student.status == 'PRESENT'
-                                              ? Icons.check_circle
-                                              : Icons.cancel,
-                                          color: student.status == 'PRESENT'
-                                              ? Colors.green
-                                              : Colors.red,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                Table(
+                  border: TableBorder.all(color: Colors.black),
+                  columnWidths: const {
+                    0: FractionColumnWidth(0.3), // ID column
+                    1: FractionColumnWidth(0.5), // Name column
+                    3: FractionColumnWidth(0.2), // Attendance column
+                  },
+                  children: [
+                    TableRow(
+                      decoration: BoxDecoration(color: Colors.grey[300]),
+                      children: const [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Text("MSSV",
+                                  textAlign: TextAlign.center,
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
                             ),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Text("Họ và Tên",
+                                  textAlign: TextAlign.center,
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Text("Trạng thái",
+                                  textAlign: TextAlign.center,
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Populate rows dynamically
+                    for (var student in attendanceRecords)
+                      TableRow(
+                        children: [
+                          TableCell(
+                            verticalAlignment:
+                                TableCellVerticalAlignment.middle,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Text(student.studentId,
+                                    textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                          TableCell(
+                            verticalAlignment:
+                                TableCellVerticalAlignment.middle,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Text(
+                                    classProvider
+                                        .findNameById(student.studentId),
+                                    textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                          TableCell(
+                            verticalAlignment:
+                                TableCellVerticalAlignment.middle,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: GestureDetector(
+                                  onTap: () async {
+                                    String newStatus =
+                                        student.status == 'PRESENT'
+                                            ? 'EXCUSED_ABSENCE'
+                                            : 'PRESENT';
+                                    await rollCallProvider.setAttendanceStatus(
+                                        student.attendanceId, newStatus);
+                                    setState(() {
+                                      student.status = newStatus;
+                                    });
+                                  },
+                                  child: Icon(
+                                    student.status == 'PRESENT'
+                                        ? Icons.check_circle
+                                        : Icons.cancel,
+                                    color: student.status == 'PRESENT'
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                    ],
-                  ),
+                  ],
                 ),
-                if (_isLoading)
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16.0),
-                    child: CircularProgressIndicator(),
-                  ),
               ],
             ),
+          ),
+          if (_isLoading)
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
     );
   }
 
